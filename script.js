499d5b31e5172fc26b7fbafc8fa3bd675a88eafc
Israel Oliveira <contato@raloliver.com>
2016-06-16 14:01:24 -0300

    ng-transclude

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-var app = angular.module('leadlovers', [])
-app.controller('collapseToogle', function ($scope) {
-    $scope.collapseHide = false;
-    $scope.collapseShow = function () {
-        $scope.collapseHide = $scope.collapseHide ? false : true;
-    }
-});
+angular.module('leadlovers', [])
+    .directive('collapsible', collapsibleBlock);
+        function collapsibleBlock(){  
+            function collapsibleLink(scope, element, attrs){
+                scope.collapsed = true;
+                scope.title = attrs.collapsibleTitle;
+                scope.collapsedIcon = attrs.collapsedIcon;
+                scope.expandedIcon = attrs.expandedIcon;
+
+                scope.toggle = function collapsibleToggle(e){
+                e.preventDefault();
+                scope.collapsed = !scope.collapsed;
+                };
+            }
+
+            return {
+                scope: {},
+                restrict: 'E',
+                replace: true,
+                transclude: true,
+                template: '<section class="clps" ng-class="{\'clps--expanded\': collapsed}">
+                            <a href="#" class="clps__title" ng-click="toggle($event)">
+                            <i class="{{collapsedIcon}}" ng-if="collapsedIcon && collapsed"></i>
+                            <i class="{{expandedIcon}}" ng-if="expandedIcon && !collapsed"></i>
+                            <span>{{title}}</span>
+                            </a>
+
+                            <section ng-transclude class="clps__content" ng-show="!collapsed"></section>
+                        </section>',
+                link: collapsibleLink
+            }
+        }
