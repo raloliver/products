01c46f590ded955cdb7f700d6544b4ee74dc6125
Israel Oliveira <contato@raloliver.com>
2016-06-16 18:45:45 -0300

    menu-lateral

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
 angular.module('leadlovers', [])
-    .directive('collapsible', collapsibleBlock);
-        function collapsibleBlock(){  
-            function collapsibleLink(scope, element, attrs){
-                scope.collapsed = true;
-                scope.title = attrs.collapsibleTitle;
-                scope.collapsedIcon = attrs.collapsedIcon;
-                scope.expandedIcon = attrs.expandedIcon;
-
-                scope.toggle = function collapsibleToggle(e){
-                e.preventDefault();
-                scope.collapsed = !scope.collapsed;
-                };
-            }
-
-            return {
-                scope: {},
-                restrict: 'E',
-                replace: true,
-                transclude: true,
-                template: '<section class="clps" ng-class="{\'clps--expanded\': collapsed}">
-                            <a href="#" class="clps__title" ng-click="toggle($event)">
-                            <i class="{{collapsedIcon}}" ng-if="collapsedIcon && collapsed"></i>
-                            <i class="{{expandedIcon}}" ng-if="expandedIcon && !collapsed"></i>
-                            <span>{{title}}</span>
-                            </a>
-
-                            <section ng-transclude class="clps__content" ng-show="!collapsed"></section>
-                        </section>',
-                link: collapsibleLink
-            }
-        }
+.controller('accordion', function($scope) {
+  $scope.expand = false;
+})
+.directive("toggleclass", function () {
+  return {
+    restrict: 'A',
+    scope: false,
+    link: function (scope, element, attrs) {
+      element.bind('click', function() {
+        scope.$apply(function() {
+          scope.expand = !scope.expand;
+        });
+      });
+    }
+  }
+});
